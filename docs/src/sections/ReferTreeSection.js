26f154a65632670f3933acc43dac9777e41b7913
qiufh <qiufh@yonyou.com>
2017-03-07 20:27:39 +0800

    add parent children status exchange

--- docs/src/sections/ReferTreeSection.js
+++ docs/src/sections/ReferTreeSection.js
@@ -15,7 +15,8 @@ export default function ReferTreeSection() {
       <h3><Anchor id="refer-new">树形参照组件</Anchor></h3>
       <p>使用<code>referDataUrl</code>设置数据源地地。</p>
       <p>使用<code>referConditions</code>设置数据请求的参数,详见属性说明</p>
-      <p>需要引用本模块样式，如<code>import from  'ssc-refer/css/referStyle.css';</code></p>
+      <p>使用<code>defaultSelected</code>设置默认值，值为一个json数组，当是单选时只有一个item,设置为多选时为多个</p>
+      <p>需要引用本模块样式，如<code>import 'ssc-refer/css/referStyle.css';</code></p>
 
       <ReactPlayground codeText={Samples.ReferTree} />
 
