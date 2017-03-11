16c6876b9182fc2312ace19e82cfe9b905c3ba49
qiufh <qiufh@yonyou.com>
2017-03-11 15:42:55 +0800

    add ModalSpec.js

---
+++ test/server/ModalSpec.js
@@ -0,0 +1,25 @@
+import {assert} from 'chai';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import Refers from '../../src/refer/Refers.js';
+
+const referConditions = {"refCode":"user","refType":"table","displayFields":["code","name","email"]};
+const referDataUrl = "http://10.3.14.239/ficloud/refbase_ctr/queryRefJSON";
+
+describe('Refers', () => {
+  it('Should be rendered on the server side', () => {
+
+    assert.doesNotThrow(function renderOnServerSide() {
+      return ReactDOMServer.renderToString(
+        <Refers
+          labelKey="name"
+          placeholder="请选择..."
+          referConditions={referConditions}
+          referDataUrl={referDataUrl}
+          referType="list"
+        />
+      );
+    });
+  });
+});
