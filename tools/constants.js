29772f44af37b40363bc8a321fb949aed9138287
qiufh <qiufh@yonyou.com>
2017-03-20 13:41:39 +0800

    remove the css folder

--- tools/constants.js
+++ tools/constants.js
@@ -6,6 +6,5 @@ export const srcRoot = path.join(repoRoot, 'src/');
 export const distRoot = path.join(repoRoot, 'dist/');
 export const libRoot = path.join(repoRoot, 'lib/');
 export const esRoot = path.join(repoRoot, 'es/');
-export const cssRoot = path.join(repoRoot, 'css/');
 export const bowerRoot = path.join(repoRoot, 'amd/');
 export const docsRoot = path.join(repoRoot, 'docs-built/');
